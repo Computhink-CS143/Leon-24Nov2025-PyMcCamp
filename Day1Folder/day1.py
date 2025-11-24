93ec7575e1530f09d8741c23399660d60fe9f589
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-24 08:34:50 +0000

    Nov 24, 2025, 8:34 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,41 +1,188 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print ("hello from day1")
 
 ########################################################################
 # Task 1:
-
-
+# print ("Hello")
 
 ########################################################################
 # Task 2:
-
-
+# print ("""I'm Leon
+#I'm 12 years old
+#I go to Maris Stella High School (Primary)
+#If I had $2000 as a reward, I would buy lego...?""")
 
 ########################################################################
 # Task 3:
-
-
+# animal1 = "Tiger"
+# animal2 = "Elephant"
+# animal3 = "Giraffe"
+# print (animal1)
+# print (animal2)
+# print (animal3)
 
 ########################################################################
 # Task 4:
-
-
+# variable1 = 3
+# variable2 = 5
+# print (variable1 + variable2)
+# print (variable1 * variable2)
+# print (variable1 / variable2)
+# print (abs (variable1 - variable2))
 
 ########################################################################
 # Task 5:
 
+# name = "Leon"
+# food = "hamburger"
+# print ("My name is " + name)
+# print ("I like to eat " + food)
 
 
+# name = "Leon"
+# food = "hamburger"
+# print (f"""My name is {name}
+# I like to eat {food}""")
 ########################################################################
 # Task 6:
+# def intro ():
+#     print ("I'm Leon")
+#     print ("I'm 12 years old")
+#     print ("I go to Maris Stella High School (Primary)")
+#     print ("If I had $2000 as a reward, I would buy lego...?")
+    
+# intro ()
 
+
+# def intro ():
+#     print ("""I'm Leon
+# I'm 12 years old
+# I go to Maris Stella High School (Primary)
+# If I had $2000 as a reward, I would buy lego...?""")
+    
+# intro ()
 
 
 ########################################################################
 # Task 7:
+# def addition (number1, number2):
+#     print (number1 + number2)
+
+# addition (2, 3)
+# addition (243, 236)
+# addition (5000, 3)
 
 
+# def add (num1, num2):
+#     print (num1 + num2)
+
+# add (2, 3)
+# add (243, 236)
+# add (5000, 3)
 
 ########################################################################
 # Additional exercises:
+
+# Exercise 1:
+
+# Rearrange the lines of code below such that the output of the program is:
+
+## Hey
+## This is really simple but important
+## Python reads from the top to the bottom
+## Bye
+
+# print("Hey")
+# print("This is really simple but important")
+# print("Python reads from the top to the bottom")
+# print("Bye")
+
+##############################################################################
+# Exercise 2:
+
+# Store the your name in a variable called "name"
+# Print the contents of variable "name"
+
+# An example is given below
+
+# Write codes here
+greet = "hello"
+print(greet)
+
+##############################################################################
+# Exercise 3:
+
+# There are 3 variables
+## title
+## name
+## command
+   
+# title will contain the title you have, e.g Queen
+# name will contain your name, e.g. Elsa
+# command will contain the command you give to your peasants
+  
+# Do not add any additional lines to this program
+# The output of this program should be something like "Greetings Sir Ian" or "Greetings Queen Elsa"
+
+title = "Emperor"
+name = "Johnson"
+command = "eat cake"
+
+# Example Output: Emperor Johnson commands his peasants to eat cake!
+
+##############################################################################
+# Exercise 4:
+
+# write a function that prints out lines to say good bye to your friends
+
+# an example of hello is given below
+
+def hello():
+  print("Hello, how are you")
+  print("My name is Sam")
+
+hello()
+
+##############################################################################
+# Exercise 5:
+
+# For you to do:
+## Increase the value of variable A by 2.5 using the + operator
+## Double the value of variable B using the * operator
+## Print out the sum of variable A and B
+
+##############################################################################
+# Exercise 6:
+
+# Refer to the variables x and y below.
+
+# Write code to do all 4 math operations on the below variables
+# The 4 math operations are:
+# 1. addition
+# 2. subtraction
+# 3. multiplication
+# 4. division
+
+x = 5
+y = 10
+
+# Write your code below this line
+
+##############################################################################
+# Exercise 7:
+
+# Write a program to calculate how much money you get from your parents
+# a. in a month
+# b. in a year
+# c. from Primary 1 all the way to Secondary 4. (10 years)
+
+# Variables to create:
+## allowance
+## name
+
+# Print out your calculation for a, b and c
+  
+# Write your code below this line
+
+##############################################################################
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,67 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 ################# Function Section #############################
+def teleport():
+    agent.teleport_to_player()
 
+def MoveF(steps):
+    agent.move (FORWARD, steps)
+
+def MoveB(steps):
+    agent.move (BACK, steps)
+
+def MoveU(steps):
+    agent.move (UP, steps)
+
+def MoveD(steps):
+    agent.move (DOWN, steps)
+
+def MoveL(steps):
+    agent.move (LEFT, steps)
+
+def MoveR(steps):
+    agent.move (RIGHT, steps)
+
+def TurnL():
+    agent.turn (LEFT)
+
+def TurnR():
+    agent.turn (RIGHT)
+
+def obby1 ():
+    agent.move (FORWARD, 4)
+    agent.move (LEFT, 4)
+    agent.move (FORWARD, 3)
+    for i in range (3):
+        agent.move (UP, 1)
+        agent.move (FORWARD, 1)
+    agent.move (FORWARD, 1)
+    for i in range (3):
+        agent.move (FORWARD, 1)
+        agent.move (DOWN, 1)
+
+def move(fb, ud, lr, blks):
+    if fb == 2:
+        agent.move (FORWARD, blks)
+    elif fb == 1:
+        agent.move (BACK, blks)
+    
+    if ud == 2:
+        agent.move (UP, blks)
+    elif ud == 1:
+        agent.move (DOWN, blks)
+    
+    if lr == 2:
+        agent.move (LEFT, blks)
+    elif lr == 1:
+        agent.move (RIGHT, blks)
+
+def turn (lr, times):
+    if lr == 1:
+        for i in range (times):
+            agent.turn (LEFT)
+    elif lr == 0:
+        for i in range (times):
+            agent.turn (RIGHT)
 
 ################## On Chat Commands Section #####################
