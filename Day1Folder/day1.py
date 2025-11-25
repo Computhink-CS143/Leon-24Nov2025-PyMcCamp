5a09ef74d666f3ba857a4102273c8bd42ce126bd
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-25 06:17:39 +0000

    Nov 25, 2025, 6:17 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -137,7 +137,7 @@
 title = "Emperor"
 name = "Leon"
 command = "grow wheat"
-print ("")
+print (f"{title, name}")
 
 ##############################################################################
 # Exercise 4:
