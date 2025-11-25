7454f9e933608995c49986b294c985f6ab32a669
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-25 07:09:54 +0000

    Nov 25, 2025, 7:09 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -214,5 +214,5 @@ allownace = f"{allowance:.2f}"
 name = "Leon"
 print (f"{name} has an allowance of ${allowance * 28} - ${allowance *31} in a month.")
 print (f"{name} has an allowance of ${allowance * 365} in a year.")
-print (f"{name} has a total allowance of $")
+print (f"{name} has a total allowance of ${allowance * 365 * }")
 ##############################################################################
