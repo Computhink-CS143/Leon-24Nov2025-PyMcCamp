638f3a1c87b95742fb0a830d38363cdd8566c64e
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-26 06:15:14 +0000

    Nov 26, 2025, 6:15 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -6,7 +6,7 @@
 # Task 1:
 name = input ("What is your name? ")
 title = input ("What is your title? ")
-command = input ("What command do you want to give to your pheasants? ")
+command = input ("What command do you want to give to your peasants? ")
 print (f"{title} {name} commands")
 
 
