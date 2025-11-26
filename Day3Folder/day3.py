554a344ab5ff7643d1ae08e822ba685594a3b8fe
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-26 06:33:54 +0000

    Nov 26, 2025, 6:33 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -22,8 +22,8 @@ num_pens = 20
 print (name + " bought " + str (num_pens) + " pens")
 
 name = input ("What is your name? ")
-num_pens = 20
-print (name + " bought " + str (num_pens) + " pens")
+num_pens = input ("How many pens did you buy? ")
+print (name + " bought " + num_pens + " pens")
 
 name = "Leon"
 num_pens = 20
