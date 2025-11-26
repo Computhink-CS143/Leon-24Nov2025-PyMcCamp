03e7e46ac64af66a7065f2ea0bee12ad25f4ba55
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-26 06:18:58 +0000

    Nov 26, 2025, 6:18 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -85,6 +85,7 @@
 # Write a program to New Year Eve CountDown Program
 # count down from 10 to 0 and say "Happy New Year!" at the end
 
+for i in range (10, -1, )
 ##############################################################################
 # Exercise 2:
 
